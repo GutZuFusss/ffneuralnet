7f4ce035cd955789daa7c82fe51b5a9689390469
GutZuFusss <leon.ikinger@googlemail.com>
2020-05-19 21:23:44 +0200

    added everything needed for testing... so far seems to work fine

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <time.h>
+#include <vector>
+#include "NNetwork.h"
 
 int main(int argc, char** argv)
 {
-	printf("We good\n");
+	srand(time(NULL)); // switch to c++11's random library
+
+	std::vector<unsigned int> structure = {2, 4, 1};
+	NNetwork net(structure);
+	std::vector<double> inputs(structure[0], 1);
+
+	printf("inputs: ");
+	for(int i = 0; i < inputs.size(); i++)
+		printf("%f, ", inputs[i]);
+	printf("\n\n\n");
+
+	net.feedForward(inputs);
+
 	system("pause");
 }
