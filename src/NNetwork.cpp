d4e3720c54ab604ddccbd4d2321436028e6c9403
GutZuFusss <leon.ikinger@googlemail.com>
2020-05-19 21:10:23 +0200

    implemented the last needed function... ready to test!

--- src/NNetwork.cpp
+++ src/NNetwork.cpp
@@ -9,14 +9,19 @@ NNetwork::NNetwork(const std::vector<unsigned int> &networkStructure)
 
 		// now populate layer with neurons (and add synapses to the neurons)
 		unsigned int neededSynapses = (l == networkStructure.size() - 1) ? 0 : networkStructure[l + 1]; // don't need any synapses for the output layer
-		for (unsigned int n = 0; n < networkStructure[l]; n++)
+		for(unsigned int n = 0; n < networkStructure[l]; n++)
 			layers[l].push_back(Neuron(n, neededSynapses));
 	}
 }
 
 std::vector<double> NNetwork::getOutput()
 {
-	// todo
+	std::vector<double> output;
+	Layer& layer = layers.back();
+	for(unsigned int n = 0; n < layer.size(); n++)
+		output.push_back(layer[n].getOutput());
+
+	return output;
 }
 
 void NNetwork::feedForward(const std::vector<double> &inputs)
