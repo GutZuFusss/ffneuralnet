9bc04afe4dfde1a284c9808f5de1e1c36c713f2c
GutZuFusss <leon.ikinger@googlemail.com>
2020-05-19 19:35:56 +0200

    spacing + parameter names

--- src/NNetwork.cpp
+++ src/NNetwork.cpp
@@ -1,6 +1,6 @@
 #include "NNetwork.h"
 
-NNetwork::NNetwork(const std::vector<unsigned int>&)
+NNetwork::NNetwork(const std::vector<unsigned int> &structure)
 {
 	// todo
 }
@@ -10,7 +10,7 @@ std::vector<double> NNetwork::getOutput()
 	// todo
 }
 
-void NNetwork::feedForward(const std::vector<double>&)
+void NNetwork::feedForward(const std::vector<double> &inputs)
 {
 	// todo
 }
--- src/NNetwork.h
+++ src/NNetwork.h
@@ -3,12 +3,12 @@
 
 class NNetwork {
 public:
-	NNetwork(const std::vector<unsigned int>&);
+	NNetwork(const std::vector<unsigned int> &);
 
 	void setWeight(unsigned int layer, unsigned int neuron, unsigned int synapse, double weight) { layers[layer][neuron].setWeight(synapse, weight); }
 	std::vector<double> getOutput();
 
-	void feedForward(const std::vector<double>&);
+	void feedForward(const std::vector<double> &);
 
 private:
 	std::vector<Layer> layers;
