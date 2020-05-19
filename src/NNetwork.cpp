b750d2f8f50c5c3e57b5e61e06cf087858f5388a
GutZuFusss <leon.ikinger@googlemail.com>
2020-05-19 19:26:18 +0200

    also modeled the NNetwork class

---
+++ src/NNetwork.cpp
@@ -0,0 +1,16 @@
+#include "NNetwork.h"
+
+NNetwork::NNetwork(const std::vector<unsigned int>&)
+{
+	// todo
+}
+
+std::vector<double> NNetwork::getOutput()
+{
+	// todo
+}
+
+void NNetwork::feedForward(const std::vector<double>&)
+{
+	// todo
+}
---
+++ src/NNetwork.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Neuron.h"
+
+
+class NNetwork {
+public:
+	NNetwork(const std::vector<unsigned int>&);
+
+	void setWeight(unsigned int layer, unsigned int neuron, unsigned int synapse, double weight) { layers[layer][neuron].setWeight(synapse, weight); }
+	std::vector<double> getOutput();
+
+	void feedForward(const std::vector<double>&);
+
+private:
+	std::vector<std::vector<Neuron>> layers; // todo: typedef for std::vector<Neuron> as Layer or smth
+};
