44a72c5a995f73472486b61c31f1427e94d191b9
GutZuFusss <leon.ikinger@googlemail.com>
2020-05-19 19:47:13 +0200

    made the constructor of NNetwork build our network (at least i hope so)

--- src/NNetwork.cpp
+++ src/NNetwork.cpp
@@ -1,8 +1,17 @@
 #include "NNetwork.h"
 
-NNetwork::NNetwork(const std::vector<unsigned int> &structure)
+NNetwork::NNetwork(const std::vector<unsigned int> &networkStructure)
 {
-	// todo
+	// build our neural network
+	for(unsigned int l = 0; l < networkStructure.size(); l++)
+	{
+		layers.push_back(Layer());
+
+		// now populate layer with neurons (and add synapses to the neurons)
+		unsigned int neededSynapses = (l == networkStructure.size() - 1) ? 0 : networkStructure[l + 1]; // don't need any synapses for the output layer
+		for (unsigned int n = 0; n < networkStructure[l]; n++)
+			layers[l].push_back(Neuron(n, neededSynapses));
+	}
 }
 
 std::vector<double> NNetwork::getOutput()
